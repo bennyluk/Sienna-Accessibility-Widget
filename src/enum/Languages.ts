30b059c33b3df20be62fc150c748a10c45da4574
Marko Trebizan <mtrebizan@gmail.com>
2024-05-28 20:32:25 +0200

    import also slovene language

--- src/enum/Languages.ts
+++ src/enum/Languages.ts
@@ -26,6 +26,7 @@ import pt from "../locales/pt.json";
 import ro from "../locales/ro.json";
 import ru from "../locales/ru.json";
 import sk from "../locales/sk.json";
+import si from "../locales/si.json";
 import sr from "../locales/sr.json";
 import sr_rs from "../locales/sr_rs.json";
 import ta from "../locales/ta.json";
@@ -66,6 +67,7 @@ export const LANGUAGE_DICTIONARY = {
   pt,
   ro,
   ru,
+  si,
   sk,
   sr,
   "sr-SP": sr_rs,
@@ -183,6 +185,10 @@ export const LANGUAGES: ILanguage[] = [
   {
     code: "ru",
     label: "Русский (Russian)",
+  },
+  {
+    code: "si",
+    label: "Slovenščina (Slovene)",
   },
   {
     code: "sk",

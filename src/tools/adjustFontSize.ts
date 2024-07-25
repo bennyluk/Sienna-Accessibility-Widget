6de46968ca28536484d0acda05f7b86c67223005
gubb_it <info@webfashion.in>
2024-07-25 19:50:06 +0200

    fix(Font): Adapt line-height based on font-size

--- src/tools/adjustFontSize.ts
+++ src/tools/adjustFontSize.ts
@@ -2,23 +2,37 @@ export default function adjustFontSize(multiply:number = 1) {
     document
         .querySelectorAll("h1,h2,h3,h4,h5,h6,p,a,dl,dt,li,ol,th,td,span,blockquote,.asw-text")
         .forEach((el: HTMLElement) => {
-            if(!el.classList.contains('material-icons') && !el.classList.contains('fa')) {
-                let orgFontSize = Number(el.getAttribute('data-asw-orgFontSize') ?? 0);
-    
-                if(!orgFontSize) {
-                    orgFontSize = parseInt(window.getComputedStyle(el).getPropertyValue('font-size'));
-                    el.setAttribute('data-asw-orgFontSize', String(orgFontSize));
+            const skipClasses = [
+                'material-icons',
+                'fa',
+                'fab',
+                'fas',
+                'far'
+            ];
+
+            for (const skipClass of skipClasses) {
+                if (el.classList.contains(skipClass)) {
+                    return;
                 }
-    
-                let newFontSize = orgFontSize * multiply;
-    
-                el.style['font-size'] = newFontSize + 'px';
             }
+
+            let orgFontSize = Number(el.getAttribute('data-asw-orgFontSize') ?? 0);
+
+            if (!orgFontSize) {
+                orgFontSize = parseInt(window.getComputedStyle(el).getPropertyValue('font-size'));
+                el.setAttribute('data-asw-orgFontSize', String(orgFontSize));
+            }
+
+            const newFontSize = orgFontSize * multiply;
+
+            el.style['font-size'] = newFontSize + 'px';
+            // adapt line-height accordingly to prevent text from overlapping
+            el.style['line-height'] = newFontSize * 1.2 + 'px';
         });
-    
+
     let $amount: HTMLDivElement = document.querySelector(".asw-amount");
 
-    if($amount) {
+    if ($amount) {
         $amount.innerText = `${ (multiply * 100).toFixed(0) }%`
     }
 }

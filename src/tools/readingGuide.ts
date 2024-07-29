793ba03ca5ff3ae41796cc17b811674ede379459
Bob <info@webfashion.in>
2024-07-30 01:16:56 +0200

    chore: Simplify readingGuide code

--- src/tools/readingGuide.ts
+++ src/tools/readingGuide.ts
@@ -4,33 +4,34 @@ import template from "./readingGuide.html";
 export default function readingGuide(enable=false) {
     let guide = document.querySelector('.asw-rg-container');
 
-    if(enable) {
-        if(!guide) {
-            guide = document.createElement("div");
-            guide.setAttribute('class', 'asw-rg-container')
-            guide.innerHTML = template;
-
-            let rgTop: HTMLElement = guide.querySelector('.asw-rg-top');
-            let rgBottom: HTMLElement = guide.querySelector('.asw-rg-bottom');
-            const margin = 20;
-
-            (window as any).__asw__onScrollReadableGuide = (event) => {
-                rgTop.style.height = (event.clientY - margin) + 'px'
-                rgBottom.style.height = (window.innerHeight - event.clientY - (margin * 2)) + 'px'
-            }
-
-            document.addEventListener('mousemove', (window as any).__asw__onScrollReadableGuide, { passive: false });
-            
-            document.body.appendChild(guide);
+    if (enable) {
+        if (guide) {
+            return;
         }
-    } else {
-        if(guide) {
-            guide.remove();
+        guide = document.createElement("div");
+        guide.setAttribute('class', 'asw-rg-container')
+        guide.innerHTML = template;
+
+        let rgTop: HTMLElement = guide.querySelector('.asw-rg-top');
+        let rgBottom: HTMLElement = guide.querySelector('.asw-rg-bottom');
+        const margin = 20;
+
+        (window as any).__asw__onScrollReadableGuide = (event) => {
+            rgTop.style.height = (event.clientY - margin) + 'px'
+            rgBottom.style.height = (window.innerHeight - event.clientY - (margin * 2)) + 'px'
         }
 
-        if((window as any).__asw__onScrollReadableGuide) {
-            document.removeEventListener('mousemove', (window as any).__asw__onScrollReadableGuide);
-            delete (window as any).__asw__onScrollReadableGuide;
-        }
+        document.addEventListener('mousemove', (window as any).__asw__onScrollReadableGuide, { passive: false });
+
+        document.body.appendChild(guide);
+        return;
+    }
+    if (guide) {
+        guide.remove();
+    }
+
+    if ((window as any).__asw__onScrollReadableGuide) {
+        document.removeEventListener('mousemove', (window as any).__asw__onScrollReadableGuide);
+        delete (window as any).__asw__onScrollReadableGuide;
     }
 }

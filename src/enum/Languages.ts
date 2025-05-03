1643ed0de5b47aa4148afdaa4e6d754523fb1441
BennyLuk <bennyluk.dev@gmail.com>
2025-05-03 14:19:35 -0700

    add bunch of languages

--- src/enum/Languages.ts
+++ src/enum/Languages.ts
@@ -1,168 +1,57 @@
 export const LANGUAGES: ILanguage[] = [
-  {
-    code: "ar",
-    label: "العربية (Arabic)",
-  },
-  {
-    code: "bg",
-    label: "български (Bulgarian)",
-  },
-  {
-    code: "bn",
-    label: "বাংলা (Bengali)",
-  },
-  {
-    code: "ca",
-    label: "Català (Catalan)",
-  },
-  {
-    code: "cs",
-    label: "čeština (Czech)",
-  },
-  {
-    code: "da",
-    label: "Danish (Denmark)",
-  },
-  {
-    code: "de",
-    label: "Deutsch (German)",
-  },
-  {
-    code: "el",
-    label: "Ελληνικά (Greek)",
-  },
-  {
-    code: "en",
-    label: "English (English)",
-  },
-  {
-    code: "es",
-    label: "Español (Spanish)",
-  },
-  {
-    code: "fi",
-    label: "suomi (Finnish)",
-  },
-  {
-    code: "fr",
-    label: "Français (French)",
-  },
-  {
-    code: "he",
-    label: "עברית (Hebrew)",
-  },
-  {
-    code: "hi",
-    label: "हिन्दी (Hindi)",
-  },
-  {
-    code: "hr",
-    label: "Hrvatski (Croatian)",
-  },
-  {
-    code: "hu",
-    label: "Magyar (Hungarian)",
-  },
-  {
-    code: "id",
-    label: "Bahasa Indonesia (Indonesian)",
-  },
-  {
-    code: "it",
-    label: "Italiano (Italian)",
-  },
-  {
-    code: "ja",
-    label: "日本語 (Japanese)",
-  },
-  {
-    code: "ka",
-    label: "ქართული (Georgian)",
-  },
-  {
-    code: "ko",
-    label: "한국어 (Korean)",
-  },
-  {
-    code: "lb",
-    label: "Lëtzebuergesch (Luxembourgish)",
-  },
-  {
-    code: "ms",
-    label: "Bahasa Malaysia (Malay)",
-  },
-  {
-    code: "nl",
-    label: "Nederlands (Dutch)",
-  },
-  {
-    code: "no",
-    label: "Norsk (Norwegian)",
-  },
-  {
-    code: "fa",
-    label: "فارسی (Persian)",
-  },
-  {
-    code: "pl",
-    label: "Polski (Polish)",
-  },
-  {
-    code: "pt",
-    label: "Português (Portuguese)",
-  },
-  {
-    code: "ro",
-    label: "Română (Romanian)",
-  },
-  {
-    code: "ru",
-    label: "Русский (Russian)",
-  },
-  {
-    code: "si",
-    label: "Slovenščina (Slovene)",
-  },
-  {
-    code: "sl",
-    label: "slovenščina (Slovenian)"
-  },
-  {
-    code: "sk",
-    label: "slovenčina (Slovak)",
-  },
-  {
-    code: "sr",
-    label: "Srpski (Serbian)",
-  },
-  {
-    code: "sr-SP",
-    label: "Српски (Serbian Cyrillic)",
-  },
-  {
-    code: "sv",
-    label: "Svenska (Swedish)"
-  },
-  {
-    code: "ta",
-    label: "Tagalog (Filipno)",
-  },
-  {
-    code: "tr",
-    label: "Türkçe (Turkish)",
-  },
-  {
-    code: "zh-Hans",
-    label: "繁体中文 (Traditional Chinese)",
-  },
-  {
-    code: "zh-Hant",
-    label: "繁體中文 (Traditional Chinese)",
-  },
-  {
-    code: "vi",
-    label: "Tiếng Việt (Vietnamese)",
-  }
+  { code: "am", label: "አማርኛ (Amharic)" },
+  { code: "ar", label: "العربية (Arabic)" },
+  { code: "bg", label: "български (Bulgarian)" },
+  { code: "bn", label: "বাংলা (Bengali)" },
+  { code: "ca", label: "Català (Catalan)" },
+  { code: "cs", label: "čeština (Czech)" },
+  { code: "da", label: "Danish (Denmark)" },
+  { code: "de", label: "Deutsch (German)" },
+  { code: "el", label: "Ελληνικά (Greek)" },
+  { code: "en", label: "English (English)" },
+  { code: "es", label: "Español (Spanish)" },
+  { code: "fa", label: "فارسی (Persian)" },
+  { code: "fi", label: "suomi (Finnish)" },
+  { code: "fil", label: "Tagalog (Filipno)" },
+  { code: "fr", label: "Français (French)" },
+  { code: "he", label: "עברית (Hebrew)" },
+  { code: "hi", label: "हिन्दी (Hindi)" },
+  { code: "hr", label: "Hrvatski (Croatian)" },
+  { code: "hu", label: "Magyar (Hungarian)" },
+  { code: "id", label: "Bahasa Indonesia (Indonesian)" },
+  { code: "it", label: "Italiano (Italian)" },
+  { code: "ja", label: "日本語 (Japanese)" },
+  { code: "ka", label: "ქართული (Georgian)" },
+  { code: "kn", label: "ಕನ್ನಡ (Kannada)" },
+  { code: "ko", label: "한국어 (Korean)" },
+  { code: "ku", label: "Kurdî (Kurdish)" },
+  { code: "lb", label: "Lëtzebuergesch (Luxembourgish)" },
+  { code: "ml", label: "മലയാളം (Malayalam)" },
+  { code: "mn", label: "Монгол (Mongolian)" },
+  { code: "ms", label: "Bahasa Malaysia (Malay)" },
+  { code: "my", label: "မြန်မာ (Burmese)" },
+  { code: "nl", label: "Nederlands (Dutch)" },
+  { code: "no", label: "Norsk (Norwegian)" },
+  { code: "pa", label: "ਪੰਜਾਬੀ (Punjabi)" },
+  { code: "pl", label: "Polski (Polish)" },
+  { code: "pt", label: "Português (Portuguese)" },
+  { code: "ro", label: "Română (Romanian)" },
+  { code: "ru", label: "Русский (Russian)" },
+  { code: "si", label: "Slovenščina (Slovene)" },
+  { code: "sk", label: "slovenčina (Slovak)" },
+  { code: "sl", label: "slovenščina (Slovenian)" },
+  { code: "sr", label: "Srpski (Serbian)" },
+  { code: "sr-SP", label: "Српски (Serbian Cyrillic)" },
+  { code: "sv", label: "Svenska (Swedish)" },
+  { code: "sw", label: "Kiswahili (Swahili)" },
+  { code: "ta", label: "தமிழ் (Tamil)" },
+  { code: "te", label: "తెలుగు (Telugu)" },
+  { code: "th", label: "ไทย (Thai)" },
+  { code: "tr", label: "Türkçe (Turkish)" },
+  { code: "ur", label: "اردو (Urdu)" },
+  { code: "vi", label: "Tiếng Việt (Vietnamese)" },
+  { code: "zh-Hans", label: "繁体中文 (Traditional Chinese)" },
+  { code: "zh-Hant", label: "繁體中文 (Traditional Chinese)" }
 ];
 
 const locales = LANGUAGES.map(lang => lang.code);
--- src/enum/Selectors.ts
+++ src/enum/Selectors.ts
@@ -2,6 +2,6 @@ export const ALL_ELEMENT_SELECTORS = ['', '*:not(.material-icons,.asw-menu,.asw-
 
 export const LINKS_SELECTORS = ["a[href]"];
 
-export const HEADER_SELECTORS = ['h1', 'h2', 'h3', 'h4', 'h5', 'h6', '.wsite-headline', '.wsite-content-title']
+export const HEADER_SELECTORS = ['h1', 'h2', 'h3', 'h4', 'h5', 'h6']
 
 export const TEXT_SELECTORS = [...HEADER_SELECTORS , 'img', 'p', 'i', 'svg', 'a', 'button:not(.asw-btn)', 'label', 'li', 'ol'];

550162b0aaf991696363562bcae1c4a411771bf1
rosarioruta <rosario.ruta@digited.it>
2025-06-06 15:32:02 +0200

    feat: initialize also if readystatechange already completed

--- src/entry.ts
+++ src/entry.ts
@@ -25,6 +25,11 @@ async function initialize() {
     }
 }
 
-// Use readystatechange for async support
-document.addEventListener("readystatechange", initialize);
+if (document.readyState === 'complete' || document.readyState === 'interactive') {
+    // Initialize if the script is appended to the DOM when document.readyState is completed
+    initialize();
+}else{
+    // Use readystatechange for async support
+    document.addEventListener("readystatechange", initialize);
+}
 

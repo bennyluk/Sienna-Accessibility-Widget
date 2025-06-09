6f03ae2cbd33fc6954c37c70b05b0880c8b0be76
rosarioruta <rosario.ruta@digited.it>
2025-06-09 15:41:49 +0200

    fix: first click problem on close button

--- src/views/menu/toggleMenu.ts
+++ src/views/menu/toggleMenu.ts
@@ -1,5 +1,5 @@
 import { $menu } from "./menu";
 
 export default function toggleMenu() {
-    $menu.style.display = $menu.style.display === "block" ? "none" : "block";
+    $menu.style.display = !$menu.style.display || ($menu.style.display === "block") ? "none" : "block";
 }

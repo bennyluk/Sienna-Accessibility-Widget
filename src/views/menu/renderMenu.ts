71f2bb58fe93b11d65e3776431ec584a8738a79b
Patrik Staffler <patrik.staffler@gardenanet.it>
2025-07-17 10:09:29 +0200

    Adoption bug fix "Problem reading settings #31"

--- src/views/menu/renderMenu.ts
+++ src/views/menu/renderMenu.ts
@@ -120,6 +120,7 @@ export default function renderMenu() {
                 userSettings.states.contrast = isSelected ? key : false;
                 enableContrast(userSettings.states.contrast);
 
+				saveUserSettings();
                 return;
             }
             
